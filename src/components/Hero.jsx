b61f73265b886bb7c1988c66b535477c54782361
adamkr98 <adamkrynski2@gmail.com>
2023-09-20 17:37:10 +0200

    I added md to main page Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,53 @@
 import React from 'react';
 import ProfilePic from '../Images/AdamKrynskiPhotoProfil.jpg';
 
+// const Hero = () => {
+//   return (
+//     <div className='w-full bg-black flex flex-col items-center'>
+//         <div className='flex justify-center mt-8 mb-8'>
+//             <img src={ProfilePic} alt='Profile Picture' className='w-39 h-40 rounded-full' />
+//         </div>
+
+//         <p className='w-1/2 text-white pb-4 flex justify-center text-center'>
+//             Hi I'm Adam and I'm a Junior Front End Developper / CSS Geek.
+//         </p>
+//     </div>
+//   )
+// }
+
+// export default Hero;
+
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+
 const Hero = () => {
   return (
-    <div className='w-full bg-black flex flex-col items-center'>
-        <div className='flex justify-center mt-8 mb-8'>
-            <img src={ProfilePic} alt='Profile Picture' className='w-39 h-40 rounded-full' />
-        </div>
+  <div className='w-full bg-black flex flex-col items-center md:pt-12 md:pb-12'>
+    <div className='w-full md:w-4/5 flex flex-col md:flex-row-reverse justify-between items-center'>  
+      <div className='w-2/3 md:w-1/3 mt-4 flex flex-col items-center md:flex-row md:items-center md:justify-between'>
+        <img src={ProfilePic} alt='Profile Picture' className='w-5 rounded-md mb-4 md:mb-0' />
+      </div>
 
-        <p className='w-1/2 text-white pb-4 flex text-center'>
-            Hi I'm Kevin and I'm a Junior Front End Developper / CSS Geek.
+      <div className='md:w-2/3 md:flex-col md:items-center flex flex-col items-center justify-center'>
+        <p className='w-full text-white pb-4 ml-12 mr-12 text-center text-lg md:mr-12'>
+          Hi I'm Adam and I'm a Junior Front End Developer / CSS Geek.
         </p>
+
+        <button className='w-2/3 mx-auto text-white border border-blue-50 flex justify-center rounded-md hover:text-black hover:bg-white mb-4 md:mb-8 md:w-28 md:h-8 md:items-center'>
+          Say Hello
+        </button>
+
+        <div className='w-1/3 md:w-1/4 flex justify-between items-center mb-4'>
+          <FaGithub className="w-12 h-12 text-gray-500 hover:text-gray-700" />
+          <FaLinkedin className="w-12 h-12 text-gray-500 hover:text-gray-700" />
+        </div>
+      </div>
     </div>
+
+  </div>
+
+
+  
+
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
       {/* Right side */}
       <div className="w-full ml-4 flex justify-end">
         <ul className="w-72 flex space-x-4 items-center text-base mr-4 "> 
-          <li className='pt-0.5 pb-0.5 pl-0.5 pr-0.5 hover:border-white border-transparent border-2'>ABOUT ME</li>
+          <li className='pt-0.5 pb-0.5 pl-0.5 pr-0.5 active:border-blue-50 hover:border-white border-transparent border-2'>ABOUT ME</li>
           <li className='pt-0.5 pb-0.5 pl-0.5 pr-0.5 hover:border-white border-transparent border-2'>PROJECTS</li>
           <li className='pt-0.5 pb-0.5 pl-0.5 pr-0.5 hover:border-white border-transparent border-2'>CONTACT</li>
         </ul>
