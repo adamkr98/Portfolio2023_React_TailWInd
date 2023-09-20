16f0d14602fbcf9942216a53a681325d424cb4c5
adamkr98 <adamkrynski2@gmail.com>
2023-09-20 13:31:03 +0200

    Add profile picture plus some styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from 'react';
+import ProfilePic from '../Images/AdamKrynskiPhotoProfil.jpg';
 
 const Hero = () => {
   return (
-    <div>
-        <div>
-            
+    <div className='w-full bg-black flex flex-col items-center'>
+        <div className='flex justify-center mt-8 mb-8'>
+            <img src={ProfilePic} alt='Profile Picture' className='w-39 h-40 rounded-full' />
         </div>
 
-        <div>
-            Hi I'm Adam and I'm a Junior Front End Developper / CSS Geek.
-        </div>
+        <p className='w-1/2 text-white pb-4 flex text-center'>
+            Hi I'm Kevin and I'm a Junior Front End Developper / CSS Geek.
+        </p>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,16 +4,16 @@ const Navbar = () => {
   return (
     <div className="w-full h-12 bg-black text-white flex justify-between items-center">
       {/* Left side */}
-      <div className="w-1/2">
-        <h1 className="text-xl font-semibold ml-8">Hello</h1>
+      <div className="w-1/4">
+        <h1 className="text-xl font-semibold ml-4">Hello</h1>
       </div>
 
       {/* Right side */}
-      <div className="w-1/2 ml-4 flex justify-end">
-        <ul className="w-64 flex space-x-4 items-center text-sm mr-8 "> 
-          <li className='w-16'>ABOUT ME</li>
-          <li>PROJECTS</li>
-          <li>CONTACT</li>
+      <div className="w-full ml-4 flex justify-end">
+        <ul className="w-72 flex space-x-4 items-center text-base mr-4 "> 
+          <li className='pt-0.5 pb-0.5 pl-0.5 pr-0.5 hover:border-white border-transparent border-2'>ABOUT ME</li>
+          <li className='pt-0.5 pb-0.5 pl-0.5 pr-0.5 hover:border-white border-transparent border-2'>PROJECTS</li>
+          <li className='pt-0.5 pb-0.5 pl-0.5 pr-0.5 hover:border-white border-transparent border-2'>CONTACT</li>
         </ul>
       </div>
     </div>
