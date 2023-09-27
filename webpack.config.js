1d386002252f7d6775d3c0596f7e8f4530eeeed1
adamkr98 <adamkrynski2@gmail.com>
2023-09-27 18:39:25 +0200

    added systemvars dotenv into the webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,12 @@ module.exports = {
         stream: require.resolve('stream-browserify'),
       },
     },
+
+    plugins: [
+      // Other plugins...
+      new Dotenv({
+        systemvars: true, // Access environment variables set in the Linux environment.
+      }),
+    ],
   };
   
